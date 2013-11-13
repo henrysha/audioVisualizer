64137d729ba111088537e6144da9128afc44b43d
Seong Wook Ha <h.s.ha@ieee.org>
2013-11-12 18:54:19 -0600

    added GPL license

--- audioVisualizer.ino
+++ audioVisualizer.ino
@@ -1,3 +1,21 @@
+//audio visualizer displaying amplitudes of each audio input
+//Copyright (C) 2013 Seong Wook Ha
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const int leftInputPin = A0, rightInputPin = A1; //input pin no.
 const int leftOut[] = {0, 1, 2, 3, 4, 5}; //left output pin no.
 const int rightOut[] = {6, 7, 8, 9, 10, 11}; //right output pin no.
